ff8f057f1b0489403813e88e731bda92cab9d45a
azhi <963085900@qq.com>
2021-05-01 23:58:10 +0800

    添加push+微信推送

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let config = JSON.parse(fs.readFileSync('./config.json'));
 
 // 添加环境测试
 // process.env.COOKIE = 
+// process.env.TOKEN = 
 
 // 读取cookie
 request.defaults.headers.common.cookie = eval(config.COOKIE);
@@ -42,13 +43,38 @@ function dealData(checkInRes, statusRes){
   return {
     message: checkInRes.message,
     // checkInRec: checkInRes.list,
-    UseDays: statusRes.days,
+    useDays: statusRes.days,
     email: statusRes.email,
-    leftDays: statusRes.leftDays,
+    leftDays: parseFloat(statusRes.leftDays).toFixed(),
   }
 }
 
+function pushMessage(content){
+  return request({
+    method: 'get',
+    url: 'http://pushplus.hxtrip.com/send',
+    data: {
+      token: eval(config.TOKEN),
+      title: 'GlaDos签到结果',
+      content: content,
+      template: 'html',
+    } 
+  });
+}
 
+function prettyRes(res, during){
+  let {useDays, leftDays, message, email} = res;
+  let content = "<table border='1px solid black'><thead><tr><td>key</td><td>value</td></tr></thead><tbody>";
+  let tr1 = "<tr><td>useDays</td><td>" + useDays + "</td></tr>";
+  let tr2 = "<tr><td>leftDays</td><td>" + leftDays + "</td></tr>";
+  let tr3 = "<tr><td>email</td><td>" + email + "</td></tr>";
+  let tr4 = "<tr><td>message</td><td>" + message + "</td></tr>";
+  let tr5 = "<tr><td>cost</td><td>" + during + "</td></tr>";
+  let tail = "</tbody></table>";
+  content += (tr1 + tr2 + tr3 + tr4 + tr5 + tail);
+  
+  return content
+}
 
 async function main(){
   let start = new Date();
@@ -59,6 +85,8 @@ async function main(){
   let res = dealData(checkInRes.data, StatusRes.data.data);
   console.log(res);
   console.log("done, use " + during + "s");
+  let content = prettyRes(res, during);
+  pushMessage(content);
 }
 
 main();
